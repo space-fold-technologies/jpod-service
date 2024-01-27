9c0bb5da349d62dcb9ae827dd5963317f5ad5db4
willtanium <williamkibira@gmail.com>
2024-01-28 02:36:50 +0300

    added error handling virtual network provider

--- src/domain/networking/virtual_network_provider.cc
+++ src/domain/networking/virtual_network_provider.cc
@@ -13,45 +13,50 @@ namespace domain::networking
     virtual_network_provider::virtual_network_provider() : logger(spdlog::get("jpod"))
     {
     }
-    bool virtual_network_provider::has_interface(const std::string &name)
+    bool virtual_network_provider::has_interface(const std::string &name, std::error_code &error)
     {
-        auto socket = open_socket(AF_LOCAL);
-        ifreq request;
-        memset(&request, 0, sizeof(request));
-
-        strlcpy(request.ifr_name, name.c_str(), sizeof(request.ifr_name));
-        if (socket < 0)
+        if (auto socket = open_socket(AF_LOCAL, error); error)
         {
+            error = std::error_code(errno, std::system_category());
             return false;
         }
-        bool exists = ioctl(socket, SIOCGIFCAP, &request) >= 0;
+        else
+        {
+            ifreq request;
+            memset(&request, 0, sizeof(request));
 
-        close(socket);
-        return exists;
+            strlcpy(request.ifr_name, name.c_str(), sizeof(request.ifr_name));
+            if (socket < 0)
+            {
+                return false;
+            }
+            bool exists = ioctl(socket, SIOCGIFCAP, &request) >= 0;
+
+            close(socket);
+            return exists;
+        }
     }
     bool virtual_network_provider::create_interface(const std::string &name, const std::string &alternative, const std::vector<ip_address> &addresses, std::error_code &error)
     {
-        // int socket = open_socket(AF_INET);
-        // if (socket < 0)
-        // {
-        //     return false;
-        // }
-        // printf("OPENED SOCKET \n");
-        // ifreq request;
-        // if (!add_interface(socket, request, name, alternative))
-        // {
-        //     printf("WELL THAT DID NOT GO WELL ON THE FIRST TRY \n");
-        //     close(socket);
-        //     return false;
-        // }
-        // else if (!set_addresses(socket, alternative, command.cidr))
-        // {
-        //     printf("WELL THAT DID NOT GO WELL ON THE SECOND TRY \n");
-        //     close(socket);
-        //     //show_current_error();
-        //     return false;
-        // }
-        // close(socket);
+        if (auto socket = open_socket(AF_INET, error); socket < 0)
+        {
+            error = std::error_code(errno, std::system_category());
+            return false;
+        }
+        else
+        {
+            if (!add_interface(socket, name, alternative, error))
+            {
+                close(socket);
+                return false;
+            }
+            else if (!set_addresses(socket, alternative, addresses, error))
+            {
+                close(socket);
+                return false;
+            }
+            close(socket);
+        }
         return true;
     }
     bool virtual_network_provider::create_jail_interface(const int jail_id, std::error_code &error)
@@ -59,14 +64,21 @@ namespace domain::networking
         ifreq request;
         memset(&request, 0, sizeof(request));
         request.ifr_ifru.ifru_jid = jail_id;
-        auto socket = open_socket(AF_LOCAL);
-        if (socket < 0)
+        if (auto socket = open_socket(AF_LOCAL, error); error)
         {
+            error = std::error_code(errno, std::system_category());
             return false;
         }
-        bool created = ioctl(socket, SIOCSIFVNET, &request) >= 0;
-        close(socket);
-        return created;
+        else if (ioctl(socket, SIOCSIFVNET, &request) < 0)
+        {
+            error = std::error_code(errno, std::system_category());
+        }
+        else
+        {
+            close(socket);
+        }
+
+        return !error;
     }
     bool virtual_network_provider::destroy_interface(const std::string &name, std::error_code &error)
     {
@@ -74,21 +86,34 @@ namespace domain::networking
         memset(&request, 0, sizeof(request));
 
         strlcpy(request.ifr_name, name.c_str(), sizeof(request.ifr_name));
-        auto socket = open_socket(AF_LOCAL);
-        if (socket < 0)
+        if (auto socket = open_socket(AF_LOCAL, error); error)
         {
+            error = std::error_code(errno, std::system_category());
             return false;
         }
-        bool destroyed = ioctl(socket, SIOCIFDESTROY, &request) >= 0;
-
-        close(socket);
-        return destroyed;
+        else if (ioctl(socket, SIOCIFDESTROY, &request) < 0)
+        {
+            error = std::error_code(errno, std::system_category());
+        }
+        else
+        {
+            close(socket);
+        }
+        return !error;
     }
 
     //
-    int virtual_network_provider::open_socket(const int address_family)
+    int virtual_network_provider::open_socket(const int address_family, std::error_code &error)
     {
-        return socket(address_family, SOCK_DGRAM, 0);
+        if (auto fd = socket(address_family, SOCK_DGRAM, 0); fd == -1)
+        {
+            error = std::error_code(errno, std::system_category());
+            return -1;
+        }
+        else
+        {
+            return fd;
+        }
     }
     void virtual_network_provider::convert_address(sockaddr_in *in, size_t size, const std::string &address)
     {
@@ -97,34 +122,58 @@ namespace domain::networking
         inet_pton(AF_INET, address.c_str(), &in->sin_addr);
         char _ip[INET_ADDRSTRLEN];
         inet_ntop(AF_INET, &in->sin_addr, _ip, INET_ADDRSTRLEN);
-        printf("CONVERTED : %s \n", _ip);
+        logger->info("CONVERTED : {}", _ip);
     }
-    bool virtual_network_provider::add_interface(const int socket, ifreq &request, const std::string &type, const std::string &name)
+    bool virtual_network_provider::add_interface(const int socket, const std::string &type, const std::string &name, std::error_code &error)
     {
+        ifreq request;
         memset(&request, 0, sizeof(request));
         strlcpy(request.ifr_name, type.c_str(), sizeof(request.ifr_name));
 
-        printf("ADDING ADDRESS \n");
-        return ioctl(socket, SIOCIFCREATE2, &request) == 0 && rename_interface(socket, request, name);
+        logger->trace("ADDING ADDRESS");
+        if (ioctl(socket, SIOCIFCREATE2, &request) == -1)
+        {
+            error = std::error_code(errno, std::system_category());
+            return false;
+        }
+        else if (rename_interface(socket, request, name, error) == -1)
+        {
+            return false;
+        }
+        return true;
     }
-    bool virtual_network_provider::rename_interface(const int socket, ifreq &request, const std::string &name)
+    bool virtual_network_provider::rename_interface(const int socket, ifreq &request, const std::string &name, std::error_code &error)
     {
+
         std::string tmp(name);
         request.ifr_ifru.ifru_data = tmp.data();
-        printf("RENAMING INTERFACE \n");
-        return ioctl(socket, SIOCSIFNAME, &request) >= 0;
+        logger->trace("RENAMING INTERFACE");
+        if (ioctl(socket, SIOCSIFNAME, &request) == -1)
+        {
+            error = std::error_code(errno, std::system_category());
+            return false;
+        }
+        return true;
     }
-    bool virtual_network_provider::set_addresses(int socket, const std::string &name, const std::vector<ip_address> &addresses)
+    bool virtual_network_provider::set_addresses(int socket, const std::string &name, const std::vector<ip_address> &addresses, std::error_code &error)
     {
-        ifaliasreq alias_request;
-        memset(&alias_request, 0, sizeof(ifaliasreq));
+        for (const auto &address : addresses)
+        {
+            ifaliasreq alias_request;
+            memset(&alias_request, 0, sizeof(ifaliasreq));
 
-        // convert_address((struct sockaddr_in *)&alias_request.ifra_addr, sizeof(struct sockaddr_in), details.address);
-        // convert_address((struct sockaddr_in *)&alias_request.ifra_broadaddr, sizeof(struct sockaddr_in), details.broadcast);
-        // convert_address((struct sockaddr_in *)&alias_request.ifra_mask, sizeof(struct sockaddr_in), details.mask);
-        strncpy(alias_request.ifra_name, name.c_str(), IFNAMSIZ);
-        printf("SETTING ADDRESS FOR DEVICE: %s \n", alias_request.ifra_name);
-        return ioctl(socket, SIOCAIFADDR, &alias_request) == 0;
+            convert_address((struct sockaddr_in *)&alias_request.ifra_addr, sizeof(struct sockaddr_in), address.value);
+            convert_address((struct sockaddr_in *)&alias_request.ifra_broadaddr, sizeof(struct sockaddr_in), address.broadcast);
+            convert_address((struct sockaddr_in *)&alias_request.ifra_mask, sizeof(struct sockaddr_in), address.netmask);
+            strncpy(alias_request.ifra_name, name.c_str(), IFNAMSIZ);
+            logger->info("SETTING ADDRESS FOR DEVICE: {}", alias_request.ifra_name);
+            if (ioctl(socket, SIOCAIFADDR, &alias_request) - 1)
+            {
+                error = std::error_code(errno, std::system_category());
+                return false;
+            }
+        }
+        return true;
     }
     virtual_network_provider::~virtual_network_provider()
     {
