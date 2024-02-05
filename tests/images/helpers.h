994ba866a7da58d9f9e450681e1e689b35b6655f
willtanium <williamkibira@gmail.com>
2024-02-06 02:11:04 +0300

    partial skeleton for helper utility for testing

---
+++ tests/images/helpers.h
@@ -0,0 +1,15 @@
+#ifndef __JPOD_TESTING_HELPERS__
+#define __JPOD_TESTING_HELPERS__
+
+#include <cmrc/cmrc.hpp>
+#include <core/sql/data_source.h>
+#include <core/sql/migrations.h>
+
+CMRC_DECLARE(archives);
+
+namespace core::sql
+{
+    //Will need to find a way to start and stop migrations
+}
+
+#endif // __JPOD_TESTING_HELPERS__
---
+++ tests/images/test_image_store.cc
@@ -0,0 +1,13 @@
+#include <catch2/catch.hpp>
+#include <fakeit.hpp>
+#include <domain/images/sql_repository.h>
+#include <sole.hpp>
+#include <fmt/format.h>
+
+using namespace fakeit;
+using namespace domain::images;
+
+TEST_CASE("repository instruction case")
+{
+
+}
